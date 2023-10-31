7275db7567964a1dc8188cf5045e27b9d6f82215
Luis Marin <36020346+LuisMr1997@users.noreply.github.com>
2023-10-31 10:40:49 -0500

    Update 04_lists_maps_iterable.dart

--- 04_lists_maps_iterable.dart
+++ 04_lists_maps_iterable.dart
@@ -1,3 +1,4 @@
+
 void main() {
   
   final numbers = [1,2,3,4,5,5,5,6,7,8,9,9,10];
@@ -6,18 +7,32 @@ void main() {
   print('Length ${numbers.length}');
   print('Index 0: ${numbers[0]}');
   print('First: ${ numbers.first }');
-  print('Reversed: ${ numbers.reversed }');
+  
+   //Elminar los repetidos de una lista.
+   print('List original sin repetidos ${numbers.toSet().toList()}');
+  
+  
+  // se convierte en un itere
+  print('Reversed: ${ numbers.reversed }'); 
   
   final reversedNumbers = numbers.reversed;
+  
   print('Iterable: $reversedNumbers');
-  print('List: ${ reversedNumbers.toList() }');
+  
+  //Convierto el iterable en mi lista original
+  print('List: ${ reversedNumbers.toList() }'); 
+  
+  //Convierto el iterable en un objeto con valores unicos.
   print('Set: ${ reversedNumbers.toSet() }');
   
+  //Evaluamos todos los elemtnos del iterable 
   final numbersGreaterThan5 = numbers.where( (int num) {
     return num > 5; // true
   });
   
-  print('>5 iterable: $numbersGreaterThan5');
-  print('>5 Set: ${ numbersGreaterThan5.toSet() }');
   
+  
+print('>5 iterable: $numbersGreaterThan5');
+print('>5 Set {}: ${ numbersGreaterThan5.toSet() }');
+  print('>5 Set []: ${ numbersGreaterThan5.toSet().toList() }');
 }
