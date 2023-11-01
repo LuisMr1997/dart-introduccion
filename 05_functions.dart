01bdd0da50c0547cbba75ebd1f3bf8b6d4f8366c
Luis Marin <36020346+LuisMr1997@users.noreply.github.com>
2023-10-31 22:30:37 -0500

    add funtions tipes params

--- 05_functions.dart
+++ 05_functions.dart
@@ -1,26 +1,34 @@
 void main() {
 
+  //Funciones fuera del main ##otros documentos.
   print( greetEveryone() );
   
+  //Funciones fuera del main con argumentos y retorna un resulado 
+  //##otros documentos.
   print( 'Suma: ${ addTwoNumbers( 10, 20 ) }' );
   
   print( greetPerson( name: 'Fernando', message: 'Hi,' ) );
   
 }
 
-
+//funcion que retorna un String y no espera paramtros
 String greetEveryone() => 'Hello everyone!';
 
+//funcion que retorna un Int y espera argumentos
 int addTwoNumbers( int a, int b ) => a + b;
 
+//funcion que retorna un Int y espera argumentos pero 
+//'b' no es obligatorio y en caso de no existir es 0
 int addTwoNumbersOptional( int a, [ int b = 0 ]) {
+//Formas de validar si existe un valor
 //   b ??= 0;
+// b = b ?? 0;
+//b++;
   return a + b;
 }
 
 
 String greetPerson({ required String name, String message = 'Hola,' }) {
-  
   return '$message Fernando';
 }
 
