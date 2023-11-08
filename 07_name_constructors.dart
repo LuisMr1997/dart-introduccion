6384cf159bd40fc6bf85c3ca770921192438b054
Luis Marin <36020346+LuisMr1997@users.noreply.github.com>
2023-11-07 20:21:22 -0500

    contructs class

--- 07_name_constructors.dart
+++ 07_name_constructors.dart
@@ -1,10 +1,14 @@
 void main() {
   
+  //Mapa de datos con claves en string y valores en dinamic (valor cualquiera)
   final Map<String, dynamic> rawJson = {
     'name': 'Tony Stark',
     'power': 'Money',
     'isAlive': true
   };
+
+  //! Hero.fromJson: Una funcion dentro de la clase
+  // 
     
 final ironman = Hero.fromJson( rawJson );
   
@@ -20,16 +24,23 @@ final ironman = Hero.fromJson( rawJson );
 }
 
 class Hero {
-  
+  //parametros de mi clase
   String name;
   String power;
   bool isAlive;
   
+  //inicializamos los valores de mi class
   Hero({
     required this.name,
     required this.power,
     required this.isAlive
   });
+
+  //Funcion dentro de una classe
+  // Creo un Map con claves en String y values en Dynamic
+  // con ?? valido que si no existe un valor ponga otro por defecto
+  //Json especifica  el formato como llega la data
+  // json['name'] significa que estamos tomando el valor de la clave del json
   
   Hero.fromJson( Map<String,dynamic> json ) 
      : name = json['name'] ?? 'No name found',
@@ -37,7 +48,7 @@ class Hero {
        isAlive = json['isAlive'] ?? 'No isAlive found';
   
 
-  
+  //Sobre escribo el metodo toString para retornarlo en la linee: 22
   @override
   String toString() {
     return '$name, $power, isAlive: ${ isAlive ? 'YES!':'Nope' }';
