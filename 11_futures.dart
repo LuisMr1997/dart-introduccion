ff8640141b09fa2c998f884dba1729da9b69286c
Luis Marin <36020346+LuisMr1997@users.noreply.github.com>
2023-11-23 20:58:40 -0500

    promesas con FEATUREs

--- 11_futures.dart
+++ 11_futures.dart
@@ -1,7 +1,16 @@
 void main() {
   
+// Un Future representa principalmente el resultado de
+// una operación asíncrona. Es una promesa de que
+// pronto tendrás un valor. La promesa puede fallar y hay
+// que manejar la excepción. Los futures son un acuerdo
+// de que en el futuro tendrás un valor para ser usado.
+
   print('Inicio del programa');
   
+
+  //Future declarado fuera del main
+  //Se utiliza como una funcion normal con Then y catch
   httpGet('https://fernando-herrera.com/cursos')
     .then( 
       (value){
@@ -15,10 +24,12 @@ void main() {
 }
 
 
+//Future inicia la promesa y regresa un tipo Dinamyc o le puedo especificar como en este caso
+
 Future<String> httpGet( String url ){
-  
+  //Feature. me muestra los metodos de feature, despues pongo una funcion () {}
   return Future.delayed( const Duration(seconds: 1), () {
-    
+   //throw genera los errores sin necesidad de return
     throw 'Error en la petición http';
     
 //     return 'Respuesta de la petición http';
